80ef443225ad67d59c83adb758bb65ed1114e80d
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-17 14:42:48 -0300

    📝 update(mobile): custom fonts and Loading component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/screens/SignIn';
+
+import { Loading } from './src/components/Loading';
 
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    );
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle="light-content"
+        translucent />
+
       <SignIn />
     </ThemeProvider>
   );
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, LoadIndicator } from './styles';
+
+export function Loading() {
+  return (
+    <Container>
+      <LoadIndicator />
+    </Container>
+  );
+}
