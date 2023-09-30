613806da4864b8831a6356daf344fd552d16b778
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-09-30 01:07:37 -0300

    📝 update(mobile): install realmDB in project

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { AppProvider, UserProvider } from '@realm/react';
 
+import { Home } from './src/screens/Home';
 import { SignIn } from './src/screens/SignIn';
-
 import { Loading } from './src/components/Loading';
 
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
 
 import { ANDROID_CLIENT_ID } from '@env';
+import { REALM_APP_ID } from '@env';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,13 +25,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        backgroundColor="transparent"
-        barStyle="light-content"
-        translucent />
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          backgroundColor="transparent"
+          barStyle="light-content"
+          translucent />
 
-      <SignIn />
-    </ThemeProvider>
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
