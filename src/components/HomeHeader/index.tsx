118546930dfb45d1333d1abc7f9a5f109338702a
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-10-11 20:30:02 -0300

    🌱 deps(mobile): icons

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,4 +1,8 @@
+import { TouchableOpacity } from 'react-native';
 import { Container, Greeting, Message, Name } from './styles';
+
+import { Power } from 'phosphor-react-native';
+import theme from '../../theme';
 
 export function HomeHeader() {
   return (
@@ -12,6 +16,10 @@ export function HomeHeader() {
           Guilherme
         </Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power color={theme.COLORS.GRAY_400} size={32} />
+      </TouchableOpacity>
     </Container>
   );
 }
