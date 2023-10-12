b4078df731def9f7e6fe6d0263eddafd692865fe
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-10-12 17:30:03 -0300

    🏦 database(mobile): method generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,8 +1,30 @@
 import { Realm } from '@realm/react';
 
-export class Historic extends Realm.Object<Historic> {
-  static generate() {
+type GenerateProps = {
+  user_id: string;
+  description: string;
+  license_plate: string;
+}
 
+export class Historic extends Realm.Object<Historic> {
+  _id!: string;
+  user_id!: string;
+  license_plate!: string;
+  description!: string;
+  status!: string;
+  created_at!: string;
+  updated_at!: string;
+
+  static generate({ user_id, license_plate, description }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      license_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date()
+    }
   }
 
   static schema = {
