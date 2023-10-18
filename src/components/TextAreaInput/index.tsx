6175e582a170feaf296d0e496c309457fdd8e726
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-10-17 23:31:50 -0300

    ✨ feat(mobile): input refs

--- src/components/TextAreaInput/index.tsx
+++ src/components/TextAreaInput/index.tsx
@@ -1,13 +1,14 @@
-import { TextInputProps } from 'react-native';
+import { TextInput, TextInputProps } from 'react-native';
 
 import { Container, Input, Label } from './styles';
 import { useTheme } from 'styled-components/native';
+import { forwardRef } from 'react';
 
 type Props = TextInputProps & {
   label: string;
 }
 
-export function TextAreaInput({ label, ...rest }: Props) {
+const TextAreaInput = forwardRef<TextInput, Props>(({ label, ...rest }, ref) => {
   const { COLORS } = useTheme();
 
   return (
@@ -20,8 +21,11 @@ export function TextAreaInput({ label, ...rest }: Props) {
         autoCapitalize="sentences"
         placeholderTextColor={COLORS.GRAY_400}
         multiline
+        ref={ref}
         {...rest}
       />
     </Container>
   );
-}
+});
+
+export { TextAreaInput };
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,3 +1,6 @@
+import { useRef } from 'react';
+import { TextInput } from 'react-native';
+
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
@@ -6,6 +9,12 @@ import { TextAreaInput } from '../../components/TextAreaInput';
 import { Container, Content } from './styles';
 
 export function Departure() {
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    console.log("Ok!");
+  }
+
   return (
     <Container>
       <Header title="Saída" />
@@ -13,15 +22,22 @@ export function Departure() {
       <Content>
         <LicensePlateInput
           label="Placa do veículo"
+          onSubmitEditing={() => descriptionRef.current?.focus()}
           placeholder="BRA1234"
+          returnKeyType="next"
         />
 
         <TextAreaInput
+          blurOnSubmit
           label="Finalidade"
+          onSubmitEditing={handleDepartureRegister}
           placeholder="Vou utilizar o veículo para..."
+          ref={descriptionRef}
+          returnKeyType="send"
         />
 
         <Button
+          onPress={handleDepartureRegister}
           title="Registrar Saída"
         />
       </Content>
