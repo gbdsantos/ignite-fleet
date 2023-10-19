1da54814d14ed2a535ef0d6af9d7b848ca24b8d2
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-10-19 20:40:25 -0300

    📝 update(mobile): display data from database

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,30 @@
 import { useNavigation } from '@react-navigation/native';
 
+import { Historic } from '../../libs/realm/schemas/Historic';
+import { useQuery } from '../../libs/realm';
+
 import { HomeHeader } from '../../components/HomeHeader';
 import { CarStatus } from '../../components/CarStatus';
 
 import { Container, Content } from './styles';
+import { useEffect } from 'react';
 
 export function Home() {
   const { navigate } = useNavigation();
 
+  // const historic = useQuery(Historic);
+
   function handleRegisterMovement() {
     navigate('departure');
   }
+
+  // function fetchVehicle() {
+  //   console.log(historic)
+  // }
+
+  // useEffect(() => {
+  //   fetchVehicle();
+  // }, []);
 
   return (
     <Container>
