97f1d5a96d8c469a4709b802c73f313866fcd72e
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-10-20 20:14:38 -0300

    ✨ feat(mobile): filter the vehicle in use

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { Historic } from '../../libs/realm/schemas/Historic';
@@ -7,9 +9,9 @@ import { HomeHeader } from '../../components/HomeHeader';
 import { CarStatus } from '../../components/CarStatus';
 
 import { Container, Content } from './styles';
-import { useEffect } from 'react';
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
   const { navigate } = useNavigation();
 
   // const historic = useQuery(Historic);
@@ -18,9 +20,15 @@ export function Home() {
     navigate('departure');
   }
 
-  // function fetchVehicle() {
-  //   console.log(historic)
-  // }
+  function fetchVehicle() {
+    try {
+      const vehicle = historic.filtered('status = "departure"')[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert('Veículo em uso', 'Não foi possível carregar o veículo em uso.');
+      console.log(error);
+    }
+  }
 
   // useEffect(() => {
   //   fetchVehicle();
@@ -32,7 +40,10 @@ export function Home() {
 
       <Content>
         <CarStatus />
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   );
