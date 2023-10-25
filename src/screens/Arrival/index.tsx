4dd268090669f17f522e25410d8f83c1cfe8593b
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-10-24 21:29:33 -0300

    📝 update(mobile): load the vehicle details

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,4 +1,8 @@
 import { useRoute } from '@react-navigation/native';
+import { BSON } from 'realm';
+
+import { useObject } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
 
 import { Button } from '../../components/Button';
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -14,6 +18,7 @@ import {
   LicensePlate
 } from './styles';
 
+
 type RouteParamsProps = {
   id: string;
 }
@@ -21,6 +26,8 @@ type RouteParamsProps = {
 export function Arrival() {
   const route = useRoute();
   const { id } = route.params as RouteParamsProps;
+
+  // const historic = useObject(Historic, new BSON.UUID(id));
 
   return (
     <Container>
@@ -32,6 +39,7 @@ export function Arrival() {
 
         <LicensePlate>
           XXX0000
+          {/* { historic?.license_plate ?? 'XXX0000' } */}
         </LicensePlate>
 
         <Label>
@@ -39,6 +47,7 @@ export function Arrival() {
         </Label>
 
         <Description>
+          {/* { historic?.description } */}
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a efficitur odio, et blandit turpis. In eget condimentum nisl, at ultrices ipsum. Etiam in aliquam ante. Phasellus nec rutrum magna. Pellentesque commodo gravida vehicula. Curabitur interdum hendrerit interdum. Pellentesque sagittis ultricies arcu at porttitor. Nam a tincidunt lectus. Fusce sollicitudin eget tortor et finibus.
         </Description>
 
