d1420fc9ffe5a2d124b62312467dbd651d031d36
Guilherme Bezerra <gbsantos.it@gmail.com>
2023-11-28 16:54:15 -0300

    📝 update(mobile): validate of coordinates

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -59,6 +59,10 @@ export function Departure() {
         return Alert.alert('Finalidade', 'Por favor, informe a finalidade da utilização do veículo.');
       }
 
+      if (!currentCoords?.latitude && !currentCoords?.longitude) {
+        return Alert.alert('Localização!', 'Não foi possível obter a localização atual. Tente novamente!');
+      }
+
       setIsRegistering(true);
 
       // realm.write(() => {
